a39eff5801df84e8b3771f40160fc01be623f92c
augustofrade <aquaaticnaotrava@gmail.com>
2025-06-23 18:02:31 -0300

    Add YouTube URL parsing & Add "youtu.be" URLs support

--- src/commands/download-video.command.ts
+++ src/commands/download-video.command.ts
@@ -4,6 +4,7 @@ import ConfigurationManager from "../core/configuration/ConfigurationManager";
 import YTDownloader from "../core/downloader/YTDownloader";
 import ConsoleLogger from "../helpers/ConsoleLogger";
 import { DownloadFlags } from "../types/interface";
+import { getYoutubeVideoId } from "../core/downloader/helpers/get-youtube-video-id";
 
 export function handleVideoDownloadCommand(argv: ArgumentsCamelCase<DownloadFlags>) {
   let saveDir = ConfigurationManager.downloadDirectory;
@@ -11,6 +12,12 @@ export function handleVideoDownloadCommand(argv: ArgumentsCamelCase<DownloadFlag
     saveDir = argv.dir;
   } else if (argv.dir && !fs.existsSync(argv.dir)) {
     ConsoleLogger.showError(`"${argv.dir}" is not a valid directory`);
+  }
+
+  const videoIdResult = getYoutubeVideoId(argv.url);
+  if (!videoIdResult.isSuccess()) {
+    ConsoleLogger.showError(videoIdResult.error);
+    return;
   }
 
   const validTypes = ["audio", "video"];
---
+++ src/core/downloader/helpers/get-youtube-video-id.ts
@@ -0,0 +1,25 @@
+import { Result } from "../../../types/result";
+
+export function getYoutubeVideoId(url: string): Result<string> {
+    let urlObj: URL;
+    try {
+        urlObj = new URL(url);
+    } catch {
+        return Result.Failure<string>(`Invalid YouTube video URL: ${url}`);
+    }
+
+    let videoId: string | null = null;
+    const pathname = urlObj.pathname.substring(1);
+
+    if(urlObj.hostname === "youtube.com" && pathname === "watch") {
+        videoId = urlObj.searchParams.get("v");
+    } else if(urlObj.hostname === "youtu.be") {
+        videoId = pathname !== "" ? pathname : null;
+    }
+
+    if(videoId) {
+        return Result.Success<string>(videoId);
+    }
+
+    return Result.Failure<string>(`Invalid YouTube video URL: ${url}`);
+}

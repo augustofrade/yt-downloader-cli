a2b1133f83132de0ff6111109897b3f81abfa6fc
augustofrade <aquaaticnaotrava@gmail.com>
2025-06-24 22:14:36 -0300

    Add music.youtube.com host availability to URL parsing

--- src/core/downloader/helpers/get-youtube-video-id.ts
+++ src/core/downloader/helpers/get-youtube-video-id.ts
@@ -1,25 +1,28 @@
 import { Result } from "../../../types/result";
 
 export function getYoutubeVideoId(url: string): Result<string> {
-    let urlObj: URL;
-    try {
-        urlObj = new URL(url);
-    } catch {
-        return Result.Failure<string>(`Invalid YouTube video URL: ${url}`);
-    }
-
-    let videoId: string | null = null;
-    const pathname = urlObj.pathname.substring(1);
-
-    if(urlObj.hostname === "youtube.com" && pathname === "watch") {
-        videoId = urlObj.searchParams.get("v");
-    } else if(urlObj.hostname === "youtu.be") {
-        videoId = pathname !== "" ? pathname : null;
-    }
-
-    if(videoId) {
-        return Result.Success<string>(videoId);
-    }
-
+  let urlObj: URL;
+  try {
+    urlObj = new URL(url);
+  } catch {
     return Result.Failure<string>(`Invalid YouTube video URL: ${url}`);
+  }
+
+  let videoId: string | null = null;
+  const pathname = urlObj.pathname.substring(1);
+
+  if (
+    (urlObj.hostname === "youtube.com" || urlObj.hostname === "music.youtube.com") &&
+    pathname === "watch"
+  ) {
+    videoId = urlObj.searchParams.get("v");
+  } else if (urlObj.hostname === "youtu.be") {
+    videoId = pathname !== "" ? pathname : null;
+  }
+
+  if (videoId) {
+    return Result.Success<string>(videoId);
+  }
+
+  return Result.Failure<string>(`Invalid YouTube video URL: ${url}`);
 }
